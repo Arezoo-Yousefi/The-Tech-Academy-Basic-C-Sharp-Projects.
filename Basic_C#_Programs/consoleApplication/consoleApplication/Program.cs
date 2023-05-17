24c63d314c714250d92836d8e4b72dd1982a126d
Arezoo-Yousefi <are_yousefi@yahoo.com>
2023-05-17 16:45:52 -0700

    console application

---
+++ Basic_C#_Programs/consoleApplication/consoleApplication/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            int resultOne = numOne * 50;
+            Console.WriteLine(resultOne);
+            Console.WriteLine("Enter another number");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+            int resultTwo = numTwo + 25;
+            Console.WriteLine(resultTwo);
+            Console.WriteLine("Enter another number");
+            int numThree = Convert.ToInt32(Console.ReadLine());
+            float resultThree= (float)(numThree /12.5);
+            Console.WriteLine(resultThree);
+            Console.WriteLine("Enter the temprature");
+            int temprature = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(temprature > 50);//check if the temprature is greater than 50;
+           
+            Console.WriteLine("Enter another number");
+            int numFour = Convert.ToInt32(Console.ReadLine());
+            int resultFour = numFour % 7;//remainder of the division by 7
+            Console.WriteLine(resultFour);
+        }
+    }
+}
---
+++ Basic_C#_Programs/simpleMath/simpleMath/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleMath
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your first number:");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your second number:");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the addition of these two numbers is: " + Convert.ToString(numberOne + numberTwo));
+            Console.WriteLine("the subtraction of these two numbers is: " + Convert.ToString(numberOne - numberTwo));
+            Console.WriteLine("the multiplication of these two numbers is: " + Convert.ToString(numberOne * numberTwo));
+            Console.WriteLine("the division of these two numbers is: " + (numberOne / numberTwo));//concatenate string and int;
+
+            Console.Read();
+        }
+    }
+}
