bd2dfc856bd993714911a8dd97fe6939995a2869
Arezoo-Yousefi <are_yousefi@yahoo.com>
2023-05-18 15:45:53 -0700

    branching assignment

---
+++ Basic_C#_Programs/BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to Package Express. Please follow the instructions below.\nPlease enter the package weight:");
+            double packageWeight = Convert.ToDouble(Console.ReadLine());
+            if (packageWeight > 50)//the package is heavier thn 50
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("Please enter the package height:");//package height
+                double packageHeight = Convert.ToDouble(Console.ReadLine());//package height
+                Console.Write("Please enter the package Width:");
+                double packageWidth = Convert.ToDouble(Console.ReadLine());//package width
+                Console.Write("Please enter the package length:");
+                double packageLength = Convert.ToDouble(Console.ReadLine());//package length
+                decimal price = (decimal)(packageWidth * packageLength * packageHeight * packageWeight / 100);
+                //var x = string.Format("{0:#.00}", price);
+                Console.WriteLine($"Your estimated total for shipping this package is: {price:#0.00} $\nThank you");//change the format to money 
+                //var x = string.Format("Your estimated total for shipping this package is: {0:#.00} $", price);
+                Console.Read();//the page is still visible untill pressing enter
+            }
+
+        }
+    }
+}
--- Basic_C#_Programs/booleanLogic/booleanLogic/Program.cs
+++ Basic_C#_Programs/booleanLogic/booleanLogic/Program.cs
@@ -10,17 +10,49 @@ namespace booleanLogic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your age?");
-            int yourAge = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Have you ever had a DUI?(yes or no answer)");
-            string yesOrNo = (Console.ReadLine()).ToLower();
-            bool haveDUI = (yesOrNo=="yes");
-            Console.WriteLine("How many speeding tickets do you have?");
-            int speedTicket = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("qualified?");
-            Console.WriteLine(yourAge > 14 && !haveDUI  && speedTicket <=3);//applicant must be over 14
-            //and must not have DUI and must not have more than 3 speed ticket
-            Console.Read();
+            //Console.WriteLine("What is your age?");
+            //int yourAge = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Have you ever had a DUI?(yes or no answer)");
+            //string yesOrNo = (Console.ReadLine()).ToLower();
+            //bool haveDUI = (yesOrNo=="yes");
+            //Console.WriteLine("How many speeding tickets do you have?");
+            //int speedTicket = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("qualified?");
+            //Console.WriteLine(yourAge > 14 && !haveDUI  && speedTicket <=3);//applicant must be over 14
+            ////and must not have DUI and must not have more than 3 speed ticket
+            //Console.Read();
+            //if and else statement
+            //Console.WriteLine("What is your Name?");
+            /*string yourName = Console.ReadLine();
+            char.ToUpper(yourName[0]);
+            
+            yourName = char.ToUpper(yourName[0])+ yourName.Substring(1);
+            Console.WriteLine(yourName);
+            if (yourName == "Arezoo" || yourName == "Ali" || yourName == "Parmida" || yourName == "Mantra")
+            {
+                Console.WriteLine("You are in the family");
+            }
+            else if(yourName != "Zohreh")
+            {
+                Console.WriteLine("You are not Arezoo's lovely sister");
+            }
+            else Console.WriteLine("You are Arezoo's sister");*/
+            const double piNumber = 3.14;
+            Console.WriteLine("enter the radius");
+            string radiusStringType = Console.ReadLine();
+            double radius = Convert.ToDouble(radiusStringType);
+            //if (radius > 0)
+            //{
+            //    Console.WriteLine("The amount of Area is " + radius * radius * piNumber);
+            //}
+            //else if (radius < 0)
+            //{
+            //    Console.WriteLine("There is no real circle");
+            //}
+            //else Console.WriteLine("this is a dot!!");
+            string result = radius > 0 ? "The amount of Area is " + radius * radius * piNumber : "There is no real circle";
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
