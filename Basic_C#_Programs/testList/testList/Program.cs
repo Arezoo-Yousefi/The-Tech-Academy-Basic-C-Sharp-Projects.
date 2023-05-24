65d453a1a0dddf3467b03b58b8f483a386ff4048
Arezoo-Yousefi <are_yousefi@yahoo.com>
2023-05-24 14:08:32 -0700

    part 2 list and array

---
+++ Basic_C#_Programs/testList/testList/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[4] { "Arezoo", "Ali", "Parmida", "Mantra" };//arrey of names
+            Console.Write("enter a number: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            if (index >= 4)
+            {
+                Console.WriteLine($"The List length is less than {index}");//Add in a message that displays when the user selects an index that doesn’t exist.
+
+            }
+            else
+            {
+                Console.WriteLine(names[index]);
+            }
+            int[] numbers = new int[10] { 23, 51, 35, 86, 14, 90, 88, 42, 69, 75 };
+            Array.Sort(numbers);
+            Console.Write("enter a number between 0 to 9: ");
+            Console.WriteLine($"The number in this index is: {numbers[Convert.ToInt32(Console.ReadLine())]}");
+
+            //Create a list of strings. Ask the user to select an index of the list and then display the content at that index on the screen.
+            List<string> LName = new List<string>();
+            bool addList = true;
+            while (addList)
+            {
+                Console.Write("enter a last name to add to the list: ");
+                string fResult = (Console.ReadLine());
+                if (fResult == "")
+                {
+                    addList = false;
+                    Console.Write($"Enter an index between 0 to {LName.Count() - 1}:");
+                    var myIndex = Console.ReadLine();
+                    int result = Convert.ToInt32(myIndex);
+                    Console.WriteLine($"Family name in this index is {LName[result]}");
+                    Console.Read();
+                }
+                LName.Add(fResult);
+            }
+        }
+    }
+}
--- Basic_C#_Programs/usingDataType/usingDataType/Program.cs
+++ Basic_C#_Programs/usingDataType/usingDataType/Program.cs
@@ -9,23 +9,27 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Academy of Learning Career College \nStudent Daily Report");
-        Console.WriteLine("What is your name ? ");
-        string YourName = Console.ReadLine();//The name is typed
-        Console.WriteLine("What course are you on? ");
-        string courseName = Console.ReadLine();//the course is typed
-        Console.WriteLine("What page number?");
-        int pageNumber = Convert.ToInt32(Console.ReadLine());//change the datatype string to integer
-        Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
-        bool anyNeeded = Convert.ToBoolean(Console.ReadLine());//change the datatype string to boolean 
-        Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics");
-        string experiance = Console.ReadLine();
-        Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
-        string feedback = Console.ReadLine();
-        Console.WriteLine("How many hours did you study today?");
-        int studyHours = Convert.ToInt32(Console.ReadLine());//change the datatype string to int
-        Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
-        Console.Read();//still stay on the page
+        decimal num1 = 8.9563m;
+        sbyte num2 = 9;
+        sbyte total = num1 + num2;
+        Console.WriteLine(total);
+        //Console.WriteLine("Academy of Learning Career College \nStudent Daily Report");
+        //Console.WriteLine("What is your name ? ");
+        //string YourName = Console.ReadLine();//The name is typed
+        //Console.WriteLine("What course are you on? ");
+        //string courseName = Console.ReadLine();//the course is typed
+        //Console.WriteLine("What page number?");
+        //int pageNumber = Convert.ToInt32(Console.ReadLine());//change the datatype string to integer
+        //Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+        //bool anyNeeded = Convert.ToBoolean(Console.ReadLine());//change the datatype string to boolean 
+        //Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics");
+        //string experiance = Console.ReadLine();
+        //Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+        //string feedback = Console.ReadLine();
+        //Console.WriteLine("How many hours did you study today?");
+        //int studyHours = Convert.ToInt32(Console.ReadLine());//change the datatype string to int
+        //Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
+        //Console.Read();//still stay on the page
 
 
     }
