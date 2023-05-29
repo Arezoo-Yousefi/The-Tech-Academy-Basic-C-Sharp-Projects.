57caaa68b87080f8a1e3235b9c6bae34b879b1f7
Arezoo-Yousefi <are_yousefi@yahoo.com>
2023-05-29 14:25:06 -0700

    third priject in this solution is about having a method with the default argument

---
+++ Basic_C#_Programs/CallingMethod/MethodAssignment/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Math
+    {
+        public int TicketTotal(int ticket, int familyMember=1)//by sefault the number of family is 1
+        {
+            int result;
+            return result = ticket * familyMember;//return the number of ticket a family needs
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallingMethod/MethodAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many tickets do you need: ");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the number of people who would like to join you: ");
+            int result = Convert.ToInt32(Console.ReadLine())+1;//total number of people
+            Math math = new Math();
+            int total;
+            if (result == 0)
+            {
+                total = math.TicketTotal(tickets);
+                Console.WriteLine($"The total amount you should pay is {total*70:00.00} $");
+            }else
+            {
+                total = math.TicketTotal(tickets, result);
+                Console.WriteLine($"The total amount you should pay is {total * 70:00.00} $");
+            }
+            Console.ReadLine();
+           
+        }
+    }
+}
