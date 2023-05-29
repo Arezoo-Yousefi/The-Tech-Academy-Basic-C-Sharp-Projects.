18f6831e3f5f1424e466da85718681343eb50213
Arezoo-Yousefi <are_yousefi@yahoo.com>
2023-05-29 13:48:19 -0700

    new project in last solution using method with different input

---
+++ Basic_C#_Programs/CallingMethod/MainMethodAssignment/MathModules.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class MathModules
+    {
+        const double pi = 3.14;
+        public double CircleArea(int radius)//if the input is integer this method is used 
+        {
+            
+            double result = Convert.ToDouble(radius * radius * pi);
+            
+            return result;
+        }
+        public decimal CircleArea(decimal radius)// if the type of the input is decimal this method is used
+        {
+            decimal result = radius * radius * Convert.ToDecimal(pi);
+            return result;
+        }
+        public string CircleArea(string radius)// if the type of the input is string this method is used
+        {
+            string result = radius + "*" + radius + "*" + Convert.ToString(pi);
+            return result;
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallingMethod/MainMethodAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathModules area = new MathModules();
+            Console.WriteLine($"The area of a Circle with the radius(3) is {area.CircleArea(3)}" +//integer input
+                $"\nThe area of a circle with the radius(3.75) is {area.CircleArea(3.75m)}" +//decimal input
+                $"\nThe area of a circle with radius(\" 3.75\") is calculated in {area.CircleArea("3.75")}");//string input
+
+            Console.ReadLine();
+        }
+    }
+}
