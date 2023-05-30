88b9b7d43588852ed092ef09a8bae113ba9f6e20
Arezoo-Yousefi <are_yousefi@yahoo.com>
2023-05-29 21:20:37 -0700

    Method class which calculate power

---
+++ Basic_C#_Programs/CallingMethod/MethodClassAssignment/MyClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class MyClass
+    {
+        public void Mathoperation(int numOne, int power)// create a void method that takes two integers as parameters
+        {
+            
+            
+            int template = numOne;
+            switch (power)//this method return a number to the power of number
+            {
+                case 0://if power is 0
+                    power = 0;
+                    numOne = 1;
+                    break;
+                case 1://if power is 1
+                    break;
+                default://if power is more than 1
+                    for (int i = 0; i <= power - 2; i++)
+                    {
+                        numOne *= template;//math operation on the first integer 
+                    }
+                    break;
+            }
+            
+            Console.WriteLine($"{template} to the power of {power} is {numOne}");
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallingMethod/MethodClassAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyClass obj = new MyClass();//instantiate the class.
+            obj.Mathoperation(3, 2);//Call the method in the class passing in two numbers
+            Console.Write("enter a number: ");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            int randomPower = random.Next(0, 10);//return a random number as the power
+            obj.Mathoperation(numberOne, randomPower);//Call the method in the class, specifying the parameters by name.
+
+            Console.Read();
+
+
+        }
+    }
+}
