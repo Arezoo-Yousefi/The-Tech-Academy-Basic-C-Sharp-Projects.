4098ddb7dc869dc5250fbc51cbf75bc7ec857508
Arezoo-Yousefi <are_yousefi@yahoo.com>
2023-06-01 16:54:48 -0700

    Lambda Expression

---
+++ Basic_C#_Programs/Module6/LambdaExpression/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<EmployeeStruct> employees = new List<EmployeeStruct>()
+            {  //create a list of at least 10 employees
+                new EmployeeStruct(){id = 5, firstName="Ali", lastName="Miri"},
+                new EmployeeStruct(){id = 7, firstName="Arezoo", lastName="Yousefi"},
+                new EmployeeStruct(){id = 10, firstName="Parmida", lastName="Miri"},
+                new EmployeeStruct(){id = 11, firstName="Mantra", lastName="Miri"},
+                new EmployeeStruct(){id = 6, firstName="Ali", lastName="Rezayee"},
+                new EmployeeStruct(){id = 17, firstName="Joe", lastName="wolfe"},
+                new EmployeeStruct(){id = 18, firstName="Alex", lastName="Soofi"},
+                new EmployeeStruct(){id = 9, firstName="Eden", lastName="Hamidi"},
+                new EmployeeStruct(){id = 20, firstName="Ali", lastName="Hasani"},
+                new EmployeeStruct(){id = 1, firstName="Joe", lastName="Ashgar"},
+                new EmployeeStruct(){id = 22, firstName="Arezoo", lastName="Noodi"}
+            };
+            List<EmployeeStruct> employeesJoe = new List<EmployeeStruct>();
+            foreach(EmployeeStruct emp in employees)
+            { //Using a foreach loop, create a new list of all employees with the first name “Joe”.
+                if (emp.firstName == "Joe")
+                {
+                    employeesJoe.Add(emp);
+                }
+            }
+            //Perform the same action again, but this time with a lambda expression
+            List<EmployeeStruct> employeeAlis = employees.Where(emp => emp.firstName == "Ali").ToList();
+            int sum = employeeAlis.Count();
+            Console.WriteLine(sum);
+
+            List<EmployeeStruct> employeeIdgreaterthan5 = new List<EmployeeStruct>();
+
+            //make a list of all employees with an Id number greater than 5.
+            employeeIdgreaterthan5 = (employees.Where(emp => emp.id > 5)).ToList();
+            int sum2 = employeeIdgreaterthan5.Count();
+            Console.WriteLine($"The number of employees with id greater than 5 is {sum2}");
+            Console.Read();
+            
+
+
+        }
+    }
+}
--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -10,12 +10,14 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+
+
             
-            Card card1 = new Card();
-            Card card2 = card1;
-            card1.Face = faceEnum.King;
-            card2.Face = faceEnum.Ace;
-            Console.WriteLine(card1.Face);
+            //Card card1 = new Card();
+            //Card card2 = card1;
+            //card1.Face = faceEnum.King;
+            //card2.Face = faceEnum.Ace;
+            //Console.WriteLine(card1.Face);
 
             //Card card1 = new Card();
             //card1.Suit = suitEnum.Clubs;
@@ -29,12 +31,25 @@ namespace TwentyOne
             //game -= player;
 
             Deck deck = new Deck();
-            deck.Shuffle(3);
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " " + card.Suit);
 
-            }
+            int count = deck.Cards.Count(x => x.Face == faceEnum.Five);
+            //List<Card> newList = deck.Cards.Where(x => x.Face == faceEnum.King).ToList();
+            List<int> numberList = new List<int>() { 2, 5, 234, 532, 1, 78, 346, 7 };
+            int sum = numberList.Where(x=>x<79).Sum();
+
+            //foreach(Card card in newList)
+            //{
+            //    Console.WriteLine(card.Face);
+            //}
+
+
+            Console.WriteLine(sum);
+            //deck.Shuffle(3);
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " " + card.Suit);
+
+            //}
             //Console.WriteLine($"The total cards  are: {deck.Cards.Count}");
             //TwentyOneGame game = new TwentyOneGame();
             //game.Players = new List<string> { "Arezoo", "Ali", "Parmida" };
