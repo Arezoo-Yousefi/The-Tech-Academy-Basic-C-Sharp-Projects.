c34dac39a4c5dc738558996963235d6c757a77ec
Arezoo-Yousefi <are_yousefi@yahoo.com>
2023-05-31 17:05:36 -0700

    folder rename

---
+++ Basic_C#_Programs/Module6 - Copy/Parameters/EmployeeClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    public class EmployeeClass<T>
+    {
+        public EmployeeClass()
+        {
+            things = new List<T>();
+
+        }
+        public EmployeeClass(T[] objs)
+        {
+            things = new List<T>();
+            foreach (T obj in objs)
+            {
+                things.Add(obj);
+            }
+        }
+        public void Add(T obj)
+        {
+            things.Add(obj);
+        }
+       private List<T> things { get; set; }
+        public void Print()
+        {
+            foreach (T thing in things)
+            {
+                Console.WriteLine(thing);
+
+            }
+        }
+    }
+}
