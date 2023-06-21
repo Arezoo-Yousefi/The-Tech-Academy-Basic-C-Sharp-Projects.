9fe9e96033d73c26994021a545d149788c98c797
Arezoo-Yousefi <are_yousefi@yahoo.com>
2023-06-20 22:00:31 -0700

    Student database code first

---
+++ Basic_C#_Programs/CodeFirst/CodeFirst/Migrations/202306210407163_Arezoo.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Arezoo : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Students", "Photo");
+            DropColumn("dbo.Students", "Height");
+            DropColumn("dbo.Students", "Weight");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Students", "Weight", c => c.Single(nullable: false));
+            AddColumn("dbo.Students", "Height", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            AddColumn("dbo.Students", "Photo", c => c.Binary());
+        }
+    }
+}
---
+++ Basic_C#_Programs/CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    class Program
+    {
+        static private void AddGrades()
+        {
+            using (var ctx = new SchoolContext())
+            {
+                if (!ctx.Grades.Where(x => x.GradeId == 1).Any())
+                {
+                    var g = new Grade() { GradeId = 1, GradeName = "Grade 1", Section = "Section A" };
+                    ctx.Grades.Add(g);
+                }
+                if (!ctx.Grades.Where(x => x.GradeId == 2).Any())
+                {
+                    var g = new Grade() { GradeId = 2, GradeName = "Grade 2", Section = "Section B" };
+                    ctx.Grades.Add(g);
+                }
+                if (!ctx.Grades.Where(x => x.GradeId == 3).Any())
+                {
+                    var g = new Grade() { GradeId = 3, GradeName = "Grade 3", Section = "Section C" };
+                    ctx.Grades.Add(g);
+                }
+                ctx.SaveChanges();
+            }
+        }
+        static void Main(string[] args)
+        {
+            AddGrades();     
+            using (var ctx = new SchoolContext())
+            {
+                Console.Write("Enter a Student Name: ");//ask for a student name to add the data base
+                string studentName = Console.ReadLine();
+                Console.Write("Enter Student grade: ");
+                int studentGrade = Convert.ToInt32(Console.ReadLine());
+                var g = ctx.Grades.Where(x => x.GradeId == studentGrade).First();
+                if (g != null)
+                {
+                    var stud = new Student() { StudentName = studentName, Grade = g};
+                    ctx.Students.Add(stud);
+                    ctx.SaveChanges();
+                }
+                Console.ReadKey();
+                              
+            }
+        }
+    }
+}
